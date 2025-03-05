66cd7c9c7cd4b28e06cfb772937a2a8163600979
Hemanths2006 <149652242+Hemanths2006@users.noreply.github.com>
2025-03-05 11:31:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 var firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
+  databaseURL: "https://chaa-app-default-rtdb.firebaseio.com",
   projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
